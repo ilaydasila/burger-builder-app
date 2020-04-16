6d32f876a30fc95e729d066c1cc1fca091523381
ilaydasila <sila.kucukoz@gmail.com>
2020-04-16 19:12:38 +0200

    mobile version bg

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,6 +6,7 @@ import Logo from "../../Logo/Logo";
 import NavigationItems from "../Navigation Items/NavigationItems";
 
 const toolbar = (props) => (
+  //
   <header className={classes.Toolbar}>
     <div>MENU</div>
     <div className={classes.Logo}>
